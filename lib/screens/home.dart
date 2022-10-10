d9ecc262b98b606a2f4fa3d5ba03e912df312508
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-10-10 08:45:12 -0500

    Swiper done for home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:online_store/provider/dark_theme_provider.dart';
+import 'package:online_store/services/utils.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,22 +12,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<String> _offerImages = [
+    'assets/offers/Offer1.jpg',
+    'assets/offers/Offer2.jpg',
+    'assets/offers/Offer3.jpg',
+    'assets/offers/Offer4.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     final themState = Provider.of<DarkThemeProvider>(context);
+    Size size = Utils(context).getScreenSize;
     return Scaffold(
-      body: Center(
-        child: SwitchListTile(
-          value: themState.getDarkTheme,
-          onChanged: (bool value) {
-            setState(() {
-              themState.setDarkTheme = value;
-            });
+      body: SizedBox(
+        height: size.height * 0.30,
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return Image.asset(
+              _offerImages[index],
+              fit: BoxFit.fill,
+            );
           },
-          title: Text('Theme'),
-          secondary: Icon(themState.getDarkTheme
-              ? Icons.dark_mode_outlined
-              : Icons.light_mode_outlined),
+          autoplay: true,
+          itemCount: 4,
+          pagination: const SwiperPagination(
+            alignment: Alignment.bottomCenter,
+            builder: DotSwiperPaginationBuilder(
+                color: Colors.white, activeColor: Colors.red),
+          ),
         ),
       ),
     );
