4cfabf15aab7742b96829546b7fe9ac6a214a7bb
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-10-26 13:51:06 -0500

    Product details started

---
+++ lib/screens/product_details_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProductDetailsScreen extends StatefulWidget {
+  static const routeName = '/ProductDetails';
+  const ProductDetailsScreen({super.key});
+
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
